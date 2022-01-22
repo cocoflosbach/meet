18c475f71cf9671168a4e2fb80ffac065bfa7f28
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-01-22 13:57:35 +0100

    Use Ternary operator to show or hide details

--- src/Event.js
+++ src/Event.js
@@ -20,25 +20,45 @@ class Event extends Component {
     });
   };
   render() {
-    const event = this.props;
+    const { event } = this.props;
     const { collapsed } = this.state;
     return (
-      <div className="Events">
-        <h1 className="summary">{event.summary}</h1>
-        {/* <p className="date-timezone">
-          {event.start.dateTime}
-          {event.start.timeZone}
-        </p> */}
-        <p className="description">{event.description}</p>
-        <p className="location">{event.location}</p>
+      <ul>
+        {collapsed ? (
+          <div className="Events">
+            <h1 className="summary">{event.summary}</h1>
+            <p className="date-timezone">
+              {event.start.dateTime}
+              {event.start.timeZone}
+            </p>
+            <p className="location">{event.location}</p>
 
-        <button
-          className="details"
-          onClick={collapsed ? this.onShowDetails : this.onHideDetails}
-        >
-          {collapsed ? "Show details" : "Hide details"}
-        </button>
-      </div>
+            <button
+              className="details"
+              onClick={collapsed ? this.onShowDetails : this.onHideDetails}
+            >
+              {collapsed ? "Show details" : "Hide details"}
+            </button>
+          </div>
+        ) : (
+          <div className="Events">
+            <h1 className="summary">{event.summary}</h1>
+            <p className="date-timezone">
+              {event.start.dateTime}
+              {event.start.timeZone}
+            </p>
+            <p className="location">{event.location}</p>
+            <p className="description">{event.description}</p>
+
+            <button
+              className="details"
+              onClick={collapsed ? this.onShowDetails : this.onHideDetails}
+            >
+              {collapsed ? "Show details" : "Hide details"}
+            </button>
+          </div>
+        )}
+      </ul>
     );
   }
 }
