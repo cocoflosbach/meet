84c40dd4663015374292a13d55d559546e8b2e23
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-01-24 23:29:11 +0100

    Update EventList.js responsive grid styling

--- src/EventList.js
+++ src/EventList.js
@@ -5,7 +5,7 @@ class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <ul className="EventList">
+      <ul className="EventList grid grid-cols-1 gap-y-5 gap-x-3 sm:grid-cols-1 md:grid-cols-2 xl:gap-x-8">
         {events.map((event) => (
           <li key={event.id}>
             <Event event={event} />
