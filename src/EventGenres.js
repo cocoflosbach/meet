0f969086e1ef01bbc3c4a6d1a033b0c629751532
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-02-04 17:28:21 +0100

    Create EventGenres component for pie chart

---
+++ src/EventGenres.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+
+const EventGenres = ({ events }) => {
+  const [data, setData] = useState([]);
+  const COLORS = ["#2E1042", "#FC3D5F", "#6FCDB9", "#804AAA", "#FCCB21"];
+
+  useEffect(() => {
+    setData(() => getData());
+  }, [events]);
+
+  const getData = () => {
+    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+    const data = genres.map((genre) => {
+      const value = events.filter((event) =>
+        event.summary.split(" ").includes(genre)
+      ).length;
+      return { name: genre, value };
+    });
+    return data;
+  };
+
+  return (
+    <ResponsiveContainer height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          innerRadius={20}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+          label={({ name, percent }) =>
+            `${name} ${(percent * 100).toFixed(0)}%`
+          }
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenres;
