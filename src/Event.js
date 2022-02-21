287f51e7721c5d4ec40f1339014a92229e733723
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-02-21 09:59:37 +0100

    Update event styling

--- src/Event.js
+++ src/Event.js
@@ -23,14 +23,15 @@ class Event extends Component {
   render() {
     const { event } = this.props;
     const { collapsed } = this.state;
+    const date = new Date(event.start.dateTime);
     return (
-      <div className="container bg-white shadow-2xl max-w-2xl mx-auto px-4 sm:py-8 sm:px-6  lg:px-8">
+      <div className="container bg-white max-w-2xl mx-auto px-4 sm:py-8 sm:px-6  lg:px-8">
         {collapsed ? (
           <div className="Events shadow-lg w-full min-h-80 bg-white-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
             <div className="text-left px-2 py-4">
               <h1 className="summary text-2xl">{event.summary}</h1>
               <p className="date-timezone text-sm">
-                {event.start.dateTime}
+                {date}
                 {event.start.timeZone}
               </p>
               <p className="location">{event.location}</p>
@@ -58,9 +59,9 @@ class Event extends Component {
               <p className="description">{event.description}</p>
             </div>
 
-            <div className=" px-4 py-4 w-40 text-right">
+            <div className="button">
               <button
-                className="details shadow-md text-purple-800 "
+                className="details shadow-md text-white px-4 py-4 bg-purple-700 rounded-md mr-48 mb-4  "
                 onClick={collapsed ? this.onShowDetails : this.onHideDetails}
               >
                 {collapsed ? "Show details" : "Hide details"}
