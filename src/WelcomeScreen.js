d4175148f63696eaa84efa8218150ab29e904253
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-02-24 15:56:53 +0100

    Update WelcomeScreen.js styling

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -23,9 +23,9 @@ function WelcomeScreen(props) {
         </h4>
         <div className="button_cont static" align="center">
           <div className="google-btn">
-            <div className="google-icon-wrapper">
+            <div className="google-icon-wrapper ">
               <img
-                className="google-icon relative "
+                className="google-icon ml-3"
                 src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                 alt="Google sign-in"
               />
